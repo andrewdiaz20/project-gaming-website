501d0330485aa92a91af90a718654407af83544a
Andrew Diaz <andrewdiazn20@gmail.com>
2024-04-23 14:30:40 -0700

    GameList and GamePage

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express();
+const cookieSession = require('cookie-session')
+
+// Express Settings
+
+
+// Controllers & Routes
+
+
+// Listen for Connections 
