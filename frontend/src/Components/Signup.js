30f7cc39f41116a19d11d3125feaba2d66870811
Andrew Diaz <andrewdiazn20@gmail.com>
2024-04-23 21:31:48 -0700

    routetag

--- frontend/src/Components/Signup.js
+++ frontend/src/Components/Signup.js
@@ -29,82 +29,79 @@ function Signup() {
             }),
         })
             .then((response) => response.json())
-            .then((data) => {
+            .then(data) => {
                 if (data.status === 'ok') {
                     navigate('/login');
                 } else {
                     alert(data.error);
                 }
-    }
+            }
     const handleChange = (event) => {
-        const { name, value } = event.target;
-        if (name === 'username') {
-            setUsername(value);
-        } else if (name === 'email') {
-            setEmail(value);
-        } else if (name === 'password') {
-            setPassword(value);
+            const { name, value } = event.target;
+            if (name === 'username') {
+                setUsername(value);
+            } else if (name === 'email') {
+                setEmail(value);
+            } else if (name === 'password') {
+                setPassword(value);
+            }
         }
-    }
 
-    const renderSignup = () => {
+        const renderSignup = () => {
+            return (
+                <div>
+                    <h1>Signup</h1>
+                </div>
+            );
+        }
+
+        const renderLogin = () => {
+            return (
+                <div>
+                    <h1>Login</h1>
+                </div>
+            );
+        }
+
+        const renderProfile = () => {
+            return (
+                <div>
+                    <h1>Profile</h1>
+                </div>
+            );
+        }
+
+        const renderError = () => {
+            return (
+                <div>
+                    <h1>Error</h1>
+                </div>
+            );
+        }
+
+        const renderPage = () => {
+            if (page === 'signup') {
+                return renderSignup();
+            } else if (page === 'login') {
+                return renderLogin();
+            } else if (page === 'profile') {
+                return renderProfile();
+            } else if (page === 'error') {
+                return renderError();
+            }
+        }
+
+        const page = 'signup';
+
+        renderPage();
+
+
         return (
             <div>
                 <h1>Signup</h1>
             </div>
         );
     }
-
-    const renderLogin = () => {
-        return (
-            <div>
-                <h1>Login</h1>
-            </div>
-        );
-    }
-
-    const renderProfile = () => {
-        return (
-            <div>
-                <h1>Profile</h1>
-            </div>
-        );
-    }
-    
-    const renderError = () => {
-        return (
-            <div>
-                <h1>Error</h1>
-            </div>
-        );
-    }
-
-    const renderPage = () => {
-        if (page === 'signup') {
-            return renderSignup();
-        } else if (page === 'login') {
-            return renderLogin();
-        } else if (page === 'profile') {
-            return renderProfile();
-        } else if (page === 'error') {
-            return renderError();
-        }
-    }
-
-    const page = 'signup';
-
-    renderPage();
-
-
-    return (
-        <div>
-            <h1>Signup</h1>
-        </div>
-    );
 }
 
-<<<<<<< HEAD
-export default SignUp;
-=======
-export default Signup;
->>>>>>> 4cabcaa6778c9e4a84c1f4654fa4bbdf3b2cbe8b
+    export default SignUp;
