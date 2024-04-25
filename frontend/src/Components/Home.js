56a0d05e2cc64cbd93a48378c1175477383b8f3a
andrewdiaz20 <andrewdiazn20@gmail.com>
2024-04-25 12:31:35 -0700

    user-fix

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -12,7 +12,7 @@ function Home() {
             <nav class="navbar">
                 <ul class="navbar">
                     <li><a>Search</a></li>
-                    <li><a>Home</a></li>
+                    <li><a href='GameListPage'>My Games</a></li>
                     <li><a href='Login'>Login</a></li>
                     <li><a class="item2">Profile</a></li>
                 </ul>
