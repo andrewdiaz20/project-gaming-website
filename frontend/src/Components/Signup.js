5c079d64af780585994ba50677bbb4b553f8d3c0
Eyrma <erotstein@outlook,com>
2024-04-25 20:32:10 -0700

    updating the signup page

--- frontend/src/Components/Signup.js
+++ frontend/src/Components/Signup.js
@@ -1,7 +1,8 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-
-function Signup() {
-    const [username, setUsername] = useState('');
+function SignUp(props) {
+    const [username, setUserName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [firstName, setFirstName] = useState('');
@@ -15,93 +16,98 @@ function Signup() {
     }
 
     const signup = () => {
-        fetch('http://localhost:5000/signup', {
+        fetch(`${process.env.REACT_APP_BACKEND_URL}/api/user/signup`, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type': 'application/json' // Set to 'application/json'
             },
             body: JSON.stringify({
-                username: username,
-                email: email,
-                password: password,
+                username: userName,
                 firstName: firstName,
                 lastName: lastName
             }),
+            email,
+            password,
+        }),
+        .then((resp) => {
+            if (!resp.ok) {
+                //If the response status code is not OK, throw an error to catch it later
+                throw new Error('Network response was not ok');
+            }
+            return resp.json(); //Parse JSON only if the response status code is OK
         })
-            .then((response) => response.json())
-            .then(data) => {
-                if (data.status === 'ok') {
-                    navigate('/login');
-                } else {
-                    alert(data.error);
-                }
+        .then((data)) => {
+            if (data.status === 'ok') {
+                alert(data.error);
+            } else {
+                alert('Registration successful');
             }
-    const handleChange = (event) => {
-            const { name, value } = event.target;
-            if (name === 'username') {
-                setUsername(value);
-            } else if (name === 'email') {
-                setEmail(value);
-            } else if (name === 'password') {
-                setPassword(value);
-            }
-        }
-
-        const renderSignup = () => {
-            return (
-                <div>
-                    <h1>Signup</h1>
-                </div>
-            );
-        }
-
-        const renderLogin = () => {
-            return (
-                <div>
-                    <h1>Login</h1>
-                </div>
-            );
-        }
-
-        const renderProfile = () => {
-            return (
-                <div>
-                    <h1>Profile</h1>
-                </div>
-            );
-        }
-
-        const renderError = () => {
-            return (
-                <div>
-                    <h1>Error</h1>
-                </div>
-            );
-        }
-
-        const renderPage = () => {
-            if (page === 'signup') {
-                return renderSignup();
-            } else if (page === 'login') {
-                return renderLogin();
-            } else if (page === 'profile') {
-                return renderProfile();
-            } else if (page === 'error') {
-                return renderError();
-            }
-        }
-
-        const page = 'signup';
-
-        renderPage();
-
-
-        return (
-            <div>
+        })
+        .catch((err) => {
+            console.error(err);
+            alert('An error occurred during registration');
+    });
+    
+    return (
+        <div className={'mainContainer'}>
+            <div className={'titleContainer'}>
                 <h1>Signup</h1>
             </div>
-        );
-    }
+            <br />
+            <form onSubmit={handleSubmit}>
+                <div className={'inputContainer'}>
+                    <input
+                        type="text"
+                        placeholder="User Name"
+                        value={userName}
+                        onChange={(ev) => setUsername(ev.target.value)}
+                        className='inputBox' />
+                </div>
+                <div className={'inputContainer'}>
+                    <input
+                        type="text"
+                        placeholder="First Name"
+                        value={firstName}
+                        onChange={(ev) => setFirstName(ev.target.value)}
+                        className='inputBox' />
+                </div>
+                <br/>
+                <div className={'inputContainer'}>
+                    <input
+                        type="text"
+                        placeholder="Last Name"
+                        value={lastName}
+                        onChange={(ev) => setLastName(ev.target.value)}
+                        className='inputBox' />
+                </div>
+                <div className={'inputContainer'}>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(ev) => setEmail(ev.target.value)}
+                        className='inputBox' />
+                </div>
+                <div className={'inputContainer'}>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(ev) => setPassword(ev.target.value)}
+                        className='inputBox' />
+                </div>
+                <div className={'inputContainer'}> 
+                    <input
+                        type="submit"
+                        value="Create Account"
+                        className='inputButton' />
+                </div>
+                <div className='inputContainer'>
+                    Already have an account? <a href='#' onClick={(e) => { navigate('/login') }}>Login</a>
+                </div>
+            </form>
+        </div>
+    )
 }
 
-    export default SignUp;
+export default SignUp;
