07699e6f853cef90bfc3a677156548498ff88c73
Eyrma <erotstein@outlook,com>
2024-04-27 02:07:59 -0700

    created a User page

---
+++ frontend/src/Components/ReviewUserPage.js
@@ -0,0 +1,38 @@
+import Rating from '@mui/material/Rating';
+
+const data = [
+    {
+        id: 1,
+        author: 'author1',
+        game: 'game1',
+        date: 'date1',
+        rating: 1
+    },
+    {
+        id: 2,
+        author: 'author2',
+        game: 'game2',
+        date: 'date2',
+        rating: 2
+    },
+    {
+        id: 3,
+        author: 'author3',
+        game: 'game3',
+        date: 'date3',
+        rating: 3
+    }
+]
+
+
+const ReviewUserPage = () => {
+    return (
+            data.map((reviewuserpage) => (
+            <div>{reviewuserpage.author}
+            <Rating name="read-only" value={1} readOnly />
+            </div>
+            ))
+    )
+}
+
+export default ReviewUserPage;
