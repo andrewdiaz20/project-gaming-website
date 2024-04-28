a2b609accd5b269bd7fbe7e56a4e003611ccf6ae
Andrew Diaz <andrewdiazn20@gmail.com>
2024-04-28 12:15:54 -0700

    6

--- frontend/src/Components/Navigation.js
+++ frontend/src/Components/Navigation.js
@@ -7,7 +7,8 @@ function Navbar() {
             <nav className="Navbar">
                 <ul className="Navbar">
                     <li><a>Search</a></li>
-                    <li><a href='GameListPage'>My Games</a></li>
+                    <li><a href='/'>Home</a></li>
+                    <li><a href='GameList'>My Games</a></li>
                     <li><a href='Login'>Login</a></li>
                     <li><a class="item2">Profile</a></li>
                 </ul>
