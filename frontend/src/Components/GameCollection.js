546b6177969e8f02b443b242a48008e6db397047
ERotstein18 <rotstein.eyrma18@gmail.com>
2024-04-27 21:17:47 -0700

    updated

--- frontend/src/Components/GameCollection.js
+++ frontend/src/Components/GameCollection.js
@@ -1,46 +1,46 @@
-// import React from 'react';
-// // import { DataGrid } from '@mui/x-data-grid';
+import React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
 
-// const columns = [
-//   { field: 'name', headerName: 'Name', width: 130 },
-//   { field: 'description', headerName: 'Description', width: 200 },
-//   { field: 'games', headerName: 'Games', width: 200 },
-// ];
+const columns = [
+  { field: 'name', headerName: 'Name', width: 130 },
+  { field: 'description', headerName: 'Description', width: 200 },
+  { field: 'games', headerName: 'Games', width: 200 },
+];
 
-// const rows = [
-//   {
-//     id: '1',
-//     name: 'Game 1',
-//     description: 'Game 1 Description',
-//     games: '4000',
-//   },
-//   {
-//     id: '2',
-//     name: 'Game 2',
-//     description: 'Game 2 Description',
-//     games: '5000',
-//   },
-//   {
-//     id: '3',
-//     name: 'Game 3',
-//     description: 'Game 3 Description',
-//     games: '3000',
-//   },
-// ];
+const rows = [
+  {
+    id: '1',
+    name: 'Game 1',
+    description: 'Game 1 Description',
+    games: '4000',
+  },
+  {
+    id: '2',
+    name: 'Game 2',
+    description: 'Game 2 Description',
+    games: '5000',
+  },
+  {
+    id: '3',
+    name: 'Game 3',
+    description: 'Game 3 Description',
+    games: '3000',
+  },
+];
 
-// const GameCollection = () => {
-//   return (
-//     <DataGrid
-//       rows={rows}
-//       columns={columns}
-//       initialState={{
-//         pagination: {
-//           paginationModel: { page: 0, pageSize: 5 },
-//         },
-//       }}
-//       //pageSizeOptions={[5, 10]}
-//     />
-//   );
-// };
+const GameCollection = () => {
+  return (
+    <DataGrid
+      rows={rows}
+      columns={columns}
+      initialState={{
+        pagination: {
+          paginationModel: { page: 0, pageSize: 5 },
+        },
+      }}
+      //pageSizeOptions={[5, 10]}
+    />
+  );
+};
 
-// export default GameCollection;
+export default GameCollection;
--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -2,84 +2,116 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Login({ login }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [passwordError, setPasswordError] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        Login({ email, password })
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    loginUser({ email, password });
+  };
 
-    return (
-        <div>
-            <h1 class="LoginTitle">Login</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                <h1 class="SubmitButton" >Login:
-                </h1>
-                    <div className={'mainContainer'}>
-                        <div className={'titleContainer'}>
-                        </div>
-                        <br />
-                        <form onSubmit={handleSubmit}>
-                            <div className={'inputContainer'}>
-                                <input
-                                    value={email}
-                                    placeholder='Enter your email here'
-                                    onChange={(ev) => setEmail(ev.target.value)}
-                                    className='inputBox' />
-                                <label className='errorLabel'>{emailError}</label>
-                            </div>
-                            <br />
-                            <div className={'inputContainer'}>
-                                <input
-                                    value={password}
-                                    placeholder='Enter your password here'
-                                    onChange={(ev) => setPassword(ev.target.value)}
-                                    className='inputBox' />
-                                <label className='errorLabel'>{passwordError}</label>
-                            </div>
-                            <div className={'inputContainer'}>
-                                <input
-                                    type='submit'
-                                    value='Login'
-                                    className='inputButton' />
-                            </div>
-                        </form>
-                        </div>
-                </label>
-            </form>
-
-            <div class="inputContainer">
-                <a href='#' onClick={(e) => { navigate('/signup') }}>Create a new account</a>
-            </div>
-
-            <div class="loginlinks">
-                <button class="loginbuttons"onClick={() => login()}>Login with Google</button>
-                <br />
-                <button class="loginbuttons"onClick={() => login()}>Login with Facebook</button>
-                <br />
-                <button class="loginbuttons"onClick={() => login()}>Login with GitHub</button>
-                <br />
-                <button class="loginbuttons"onClick={() => login()}>Login with Twitter</button>
-                <br />
-                <button class="loginbuttons"onClick={() => login()}>Login with LinkedIn</button>
-            </div>
-
-            <div>
-                <a class="create-a-new-account" href='#' onClick={(e) => navigate('/signup')}>Create a new account</a>
-            </div>
-            <footer></footer>
+  const loginUser = ({ email, password }) => {
+    navigate('/userpage');
+    // console.log(JSON.stringify({ email, password }));
+    //fetch('${process.env.REACT_APP_BACKEND_URL}/api/user/login', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json' // Set to 'application/json'
+    //     },
+    //     body: JSON.stringify({ email, password }),
+    // })
+    //     .then((resp) => {
+    //         console.log(resp);
+    //         if (!resp.ok) {
+    //             //If the response status code is not OK, throw an error to catch it later
+    //             throw new Error('Network response was not ok');
+    //         }
+    //         return resp.json(); //Parse JSON only if the response status code is OK
+    //     })
+    //     .then((data) => {
+    //         console.log(data);
+    //         if (data.error) {
+    //             alert(data.error);
+    //         } else {
+    //             alert('Login successful');
+    //If the login was successful, set the token in local storage
+    //             login();
+    //             console.log('token from storage', localStorage.getItem('token'));
+    //             navigate('/userpage');
+    //         }
+    //     })
+    //     .catch((err) => {
+    //         console.error(err);
+    //         alert('An error occurred:');
+    //     });
+  };
+  return (
+    <div className={'mainContainer'}>
+      <div className={'titleContainer'}>
+        <h1 class="LoginTitle">Login</h1>
+      </div>
+      <br />
+      <form onSubmit={handleSubmit}>
+        <div className={'inputContainer'}>
+          <input
+            value={email}
+            placeholder="Enter your email here"
+            onChange={(ev) => setEmail(ev.target.value)}
+            className="inputBox"
+          />
+          <label className="errorLabel">{emailError}</label>
+        </div>
+        <br />
+        <div className={'inputContainer'}>
+          <input
+            value={password}
+            placeholder="Enter your password here"
+            onChange={(ev) => setPassword(ev.target.value)}
+            className="inputBox"
+          />
+          <label className="errorLabel">{passwordError}</label>
+        </div>
+        <div className={'inputContainer'}>
+          <input type="submit" value="Login" className="inputButton" />
+        </div>
+        <div class="inputContainer">
+          <a
+            href="#"
+            onClick={(e) => {
+              navigate('/signup');
+            }}
+          >
+            Create a new account
+          </a>
         </div>
 
+        <div class="loginlinks">
+          <button onClick={() => login()}>Login with Google</button>
+          <br />
+          <button onClick={() => login()}>Login with Facebook</button>
+          <br />
+          <button onClick={() => login()}>Login with GitHub</button>
+          <br />
+          <button onClick={() => login()}>Login with Twitter</button>
+          <br />
+          <button onClick={() => login()}>Login with LinkedIn</button>
+        </div>
+        <div>
+          <a
+            class="create-a-new-account"
+            href="#"
+            onClick={(e) => navigate('/signup')}
+          >
+            Create a new account
+          </a>
+        </div>
+      </form>
+    </div>
+  );
+}
 
-    )
-};
-
-
-
-export default Login
+export default Login;
