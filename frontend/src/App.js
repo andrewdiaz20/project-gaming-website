eaa727e501b2ba4aa2615d294a6a8e0054274976
yordanos woldeyes <107896300+yordanosW@users.noreply.github.com>
2024-04-28 11:49:06 -0700

    searchbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,24 @@ import React, {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import './App.css';
 import RandomGame from './Components/RandomGame';
-import Home from './Components/Home/Home';
+import Home from './Components/Home';
+import Login from './Components/Login'
+import GameListPage from './Components/GameListPage'
+
 
 function App(){
 
   return (
     <div>
       <Router>
+       
         <Home/>
           <Routes>
-              <Route path='/' element={<RandomGame />}/>
-              
+              <Route path = '/' element={<Home />}/>
+              <Route path = 'RandomGame' element={<RandomGame />}/>
+              {/* <Route path = 'GameListPage' element={<GameListPage />}/> */}
+              <Route path = 'Login' element={<Login/>}/>
+              <Route path = 'GameListPage' element={<GameListPage/>}/>
           </Routes>
          
       </Router>
